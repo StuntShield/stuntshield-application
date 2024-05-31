fe260cd285065bd8ceb4e5cbaebf6d8a6ad96d9c
Geby Ginting <gebygintingg@gmail.com>
2024-05-31 16:52:13 +0700

    Created Login Layout and some navigation

---
+++ app/src/main/java/com/geby/stuntshield/ui/auth/LoginActivity.kt
@@ -0,0 +1,33 @@
+package com.geby.stuntshield.ui.auth
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.geby.stuntshield.MainActivity
+import com.geby.stuntshield.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupAction()
+    }
+
+    private fun setupAction() {
+        binding.loginButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+}
