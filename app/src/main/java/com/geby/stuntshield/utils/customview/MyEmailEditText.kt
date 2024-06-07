9925bc00a4c1545d7ce9e486d0896bfc52529250
Geby Ginting <gebygintingg@gmail.com>
2024-06-07 16:49:47 +0700

    Created customview for email and password input edit text

---
+++ app/src/main/java/com/geby/stuntshield/utils/customview/MyEmailEditText.kt
@@ -0,0 +1,34 @@
+package com.geby.stuntshield.utils.customview
+
+import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.util.Patterns
+import androidx.appcompat.widget.AppCompatEditText
+import com.geby.stuntshield.R
+
+class MyEmailEditText @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : AppCompatEditText(context, attrs) {
+    init {
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                error = if (!Patterns.EMAIL_ADDRESS.matcher(s).matches()) {
+                    context.getString(R.string.invalid_email)
+                } else {
+                    null
+                }
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
+    }
+}
---
+++ app/src/main/java/com/geby/stuntshield/utils/customview/MyPasswordEditText.kt
@@ -0,0 +1,35 @@
+package com.geby.stuntshield.utils.customview
+
+import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.text.method.PasswordTransformationMethod
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+import com.geby.stuntshield.R
+
+class MyPasswordEditText @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : AppCompatEditText(context, attrs) {
+
+    init {
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString().length < 8) {
+                    setError(context.getString(R.string.invalid_password), null)
+                } else {
+                    error = null
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
+        transformationMethod = PasswordTransformationMethod.getInstance()
+    }
+}
